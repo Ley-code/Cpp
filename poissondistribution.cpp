4876a65b5d0b87f1fa0109f64dc0f7dbc62ec9b5
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2023-04-11 11:45:48 +0300

    Update poissondistribution.cpp

--- poissondistribution.cpp
+++ poissondistribution.cpp
@@ -2,16 +2,38 @@
 #include <math.h>
 using namespace std;
 int main(){
-    float n,x,factx=1;
+    float n,x,fact=1,lessthanorequalto = 0;
+    string chars;
     cout << "mean: ";
     cin >> n;
-    cout << "little x or events: ";
+    cout << "P( x ";
+    cin >> chars;
     cin >> x;
-    for (int i = x; i>0;i--){
-        factx *= i;  
-    }
-    float numerator = pow(n,x)*(pow(M_E, -n));
-    double p = numerator/factx;
-    cout <<"p(x)= "<<p;
+    if(chars == "="){
+        for (int i = x; i>0;i--){
+            fact *= i;  
+        }
+        float numerator = pow(n,x)*(pow(M_E, -n));
+        double pofx = numerator/fact;
+        cout <<"p(x = "<<x<<") = "<<pofx;
+    } 
+    else {
+        for(int j = x; j>=0;j--){
+            float fact2 = 1;
+            for(int i = j; i>0;i--){
+                fact2 *= i;
+            }
+            float numerator = pow(n,j)*(pow(M_E, -n));
+            double pofx2 = numerator/fact2;
+            lessthanorequalto += pofx2;
+        }
+           
 
-}
+        if (chars == ">"){
+            cout << "P( x <= " << x << ") = "<< 1-lessthanorequalto; 
+        }
+        if (chars == "<="){
+            cout << "P( x <= " << x << ") = "<< lessthanorequalto;
+        }
+    }
+} 
