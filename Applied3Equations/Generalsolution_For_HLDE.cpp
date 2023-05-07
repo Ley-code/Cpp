07d0a75706062f6fd0b230be4f6ec3afd6145c14
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2023-05-07 20:40:18 +0300

    Update Generalsolution_For_HLDE.cpp

--- Applied3Equations/Generalsolution_For_HLDE.cpp
+++ Applied3Equations/Generalsolution_For_HLDE.cpp
@@ -12,17 +12,25 @@ int main(){
     //-------------------------------------- quadratic equation solutions
     int r1 = (-b + (sqrt(pow(b,2) - (4*a*c))))/2*a;
     int r2 = (-b - (sqrt(pow(b,2) - (4*a*c))))/2*a;
-    //--------------------------------------
-    if(r1 != r2){
-        cout << "GS : c1e^(" <<r1<<"x) + c2e^("<<r2<<"x)";
-    }else if(r1 == r2){
-        cout << "GS : c1e^(" <<r1<<"x) + c2xe^("<<r2<<"x)";
-    }else if ((pow(b,2) - (4*a*c)) < 0){
-        int a = -b/(2*a);
-        int b = ((pow(b,2) - (4*a*c))*(-1)) / 
+    //-------------------------------------- 
+    if ((pow(b,2) - (4*a*c)) < 0){   //case 3, if the two roots are -ve
+        double numerator = sqrt(((pow(b,2) - (4*a*c))*(-1)));
+        double alpha = -b/(2*a);
+        double beta = numerator/(2*a);
+        if(numerator != floor(numerator)){
+            cout << "GS : c1e^(" << alpha <<"x) Cos(rootof("<< ((pow(b,2) - (4*a*c))*(-1)) << ")/" << 2*a <<")x + "; // using roots instead of calculating the root
+            cout << "c2e^(" << alpha <<"x) Sin(rootof("<< ((pow(b,2) - (4*a*c))*(-1)) << ")/" << 2*a <<")x";
+        }else {
+            cout << "GS : c1e^(" << alpha <<") Cos(" << beta << ")x + "; //when alpha and beta are integers
+            cout << "c2e^(" << alpha <<") Sin(" << beta << ")x";
+        }
+        
+    }else{  
+        if(r1 != r2){       // case 1, if both roots are real and different
+            cout << "GS : c1e^(" <<r1<<"x) + c2e^("<<r2<<"x)";
+        }else if(r1 == r2){ //case 2, if both roots are real and the same
+            cout << "GS : c1e^(" <<r1<<"x) + c2xe^("<<r2<<"x)";
+        }
     }
-   
-    
-
-    
+    //----------------------------------------
 }
