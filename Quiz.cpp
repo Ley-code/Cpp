c3bc5a071c286134ad00797d484d17d7ed921221
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2023-05-21 18:56:54 +0300

    Update Quiz.cpp

--- Quiz.cpp
+++ Quiz.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 int main(){
     int numque,numchoice;
+    int correctguesses = 0;
     string question,response;
+
     cout << "Number of questions: ";
     cin >> numque;
     cout << "Number of choices each question have: ";
@@ -24,19 +26,29 @@ int main(){
             getline(cin,choices[i][j]);
         }
         cout <<"Answer: ";
-        cin >> Answer[i];
-        cin.ignore();   
+        cin >> Answer[i]; 
+        cin.ignore(); 
+        while(Answer[i].length() >1 ){
+            cout << "Please use the character next to the choice\nAnswer: ";
+            cin>>Answer[i];
+            cin.ignore();
+            cout << endl;
+        }
     }
     //--------------------------------------------------------
-
+    for(int i=0;i<15;i++){
+        cout << endl;
+    }
     cout << "Do you wanna start the quiz(y/n): ";
     cin >> response;
+    for(int i = 0; i < response.length(); i++)
+        response[i] = response[i] + 32;
     if(response == "y"){
         cout << "-----------------------QuizTime ------------------";
         cout <<endl;
         for(int i = 0; i<numque;i++){
             cout << i+1 <<". " << Questions[i] << "?";
-            cout << endl;
+            cout << endl;                                               //asks the questions
             for(int j = 0; j<numchoice;j++){
                 cout << char(j+65) <<". " << choices[i][j];
                 cout <<endl;
@@ -44,19 +56,17 @@ int main(){
             }
             cout <<"Your Answer: ";
             cin >> guess[i];
-
+            if(Answer[i]==guess[i]){
+                cout <<"You are correct!!!"<<endl;
+                correctguesses += 1;
+            }else{
+                cout <<"You are wrong :)"<<endl;
+                cout <<"The correct answer was " << Answer[i];    
+            }
         }
     }
-    /*for(int i = 0; i< numque; i++){
-        for(int j = 0; j <numchoice; j++){
-            cout << choices[i][j] <<" ";        
-        }
-        cout << endl;
-        
-    }
-    */
-   for(int i = 0; i<numque;i++){
-    cout << Answer[i] << ", ";
-    cout << endl;
-   }
+    //---------------------------------------------------------------
+    cout <<"-------------Results!!--------------"<<endl;
+    cout << correctguesses << " out of " << numque;            //desplays the result
+    //----------------------------------------------------------------
 }
