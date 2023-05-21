075f41146ff18e8cfbaef5e66c4adf423ffda894
Leykun Birhanu <111108779+Ley-code@users.noreply.github.com>
2023-05-21 12:13:14 +0300

    Update Quiz.cpp
    Added the quiz

--- Quiz.cpp
+++ Quiz.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     int numque,numchoice;
-    string question;
+    string question,response;
     cout << "Number of questions: ";
     cin >> numque;
     cout << "Number of choices each question have: ";
@@ -11,15 +11,52 @@ int main(){
 
 
     string *Questions = new string[numque];
+    string choices[numque][numchoice];
+    string *Answer = new string[numque];
 
+    string *guess = new string[numque];
+    //-----------------------------------------------------------
     for(int i = 0; i<numque ; ++i){
         cout << "Question no " << i+1 <<": ";
         getline(cin,Questions[i]);
-        cout<< endl;
-        
+        for(int j = 0; j< numchoice; j++){
+            cout << "choice " << char(j+65) <<": ";  // taking input from the examiner
+            getline(cin,choices[i][j]);
+        }
+        cout <<"Answer: ";
+        cin >> Answer[i];
+        cin.ignore();   
+    }
+    //--------------------------------------------------------
+
+    cout << "Do you wanna start the quiz(y/n): ";
+    cin >> response;
+    if(response == "y"){
+        cout << "-----------------------QuizTime ------------------";
+        cout <<endl;
+        for(int i = 0; i<numque;i++){
+            cout << i+1 <<". " << Questions[i] << "?";
+            cout << endl;
+            for(int j = 0; j<numchoice;j++){
+                cout << char(j+65) <<". " << choices[i][j];
+                cout <<endl;
+
+            }
+            cout <<"Your Answer: ";
+            cin >> guess[i];
+
+        }
+    }
+    /*for(int i = 0; i< numque; i++){
+        for(int j = 0; j <numchoice; j++){
+            cout << choices[i][j] <<" ";        
+        }
+        cout << endl;
         
     }
-    for(int i = 0; i< numque; i++){
-        cout << Questions[i];
-    }
+    */
+   for(int i = 0; i<numque;i++){
+    cout << Answer[i] << ", ";
+    cout << endl;
+   }
 }
